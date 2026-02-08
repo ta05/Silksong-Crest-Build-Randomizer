f87201ea8ef9ace3b92c7de1ac1761b5422eb58c
Tolu Alimi <talimi2000@gmail.com>
2026-02-08 00:20:11 -0500

    Lowered the chance to get an empty slot

--- Randomizer.ts
+++ Randomizer.ts
@@ -46,6 +46,7 @@ const crestAttributes: { [key: string]: { silk: number; red: number; blue: numbe
     },
 };
 const crestNames: string[] = ["Hunter", "Reaper", "Wanderer", "Beast", "Witch", "Architect", "Shaman"];
+const silkSkills: string[] = ["Silk Spear", "Thread Storm", "Cross Stitch", "Sharp Dart", "Rune Rage", "Pale Nails"];
 const redTools: string[] = ["Straight Pin", "Threefold Pin", "Sting Shard", "Tacks", "Longpin", "Curveclaw/Curvesickle", "Throwing Ring", "Pimpillo", "Conchcutter", "Silkshot", "Delver's Drill", "Cogwork Wheel", "Cogfly", "Rosary Cannon", "Voltvessels", "Flintslate", "Snare Setter", "Flea Brew", "Plasmium Phial"];
 const blueTools: string[] = [];
 const yellowTools: string[] = [];
@@ -56,24 +57,46 @@ export const randomizeCrest = (): Crest => {
     return new Crest(randomCrestName, attributes.silk, attributes.red, attributes.blue, attributes.yellow);
 };
 
-export const randomizeTools = (crest: Crest): Crest => {
-    let shuffle = [...redTools];
-    for (let i = 0; i < crest.redTools.size; i++) {
+const randomizeTool = (crest: Crest, toolList: string[], toolKey: 'silkSkills' | 'redTools' | 'blueTools' | 'yellowTools'): Crest => {
+    let shuffle: string[] = [...toolList];
+    for (let i = 0; i < crest[toolKey].size; i++) {
         const randomIndex = Math.floor(Math.random() * (shuffle.length + 1));
         if (randomIndex < shuffle.length) {
-            crest.redTools.slots.push(new Slot(shuffle[randomIndex]));
+            crest[toolKey].slots.push(new Slot(shuffle[randomIndex]));
             shuffle = shuffle.slice(0, randomIndex).concat(shuffle.slice(randomIndex + 1));
         }
         else {
-            crest.redTools.slots.push(null);
+            if (Math.random() < 0.1) {
+                crest[toolKey].slots.push(null);
+            }
+            else {
+                i--;
+            }
         }
     }
     return crest;
 }
 
+export const randomizeTools = (crest: Crest): Crest => {
+    crest = randomizeTool(crest, silkSkills, 'silkSkills');
+    crest = randomizeTool(crest, redTools, 'redTools');
+    crest = randomizeTool(crest, blueTools, 'blueTools');
+    crest = randomizeTool(crest, yellowTools, 'yellowTools');
+    return crest;
+}
+
 export const display = (crest: Crest): void => {
     console.log(`${crest.name}`);
+    crest.silkSkills.slots.forEach((skill, index) => {
+        console.log(`    Silk Skill Slot ${index + 1}: ${skill ? skill.getName() : "Empty"}`);
+    });
     crest.redTools.slots.forEach((tool, index) => {
         console.log(`    Red Tool Slot ${index + 1}: ${tool ? tool.getName() : "Empty"}`);
+    });
+    crest.blueTools.slots.forEach((tool, index) => {
+        console.log(`    Blue Tool Slot ${index + 1}: ${tool ? tool.getName() : "Empty"}`);
+    });
+    crest.yellowTools.slots.forEach((tool, index) => {
+        console.log(`    Yellow Tool Slot ${index + 1}: ${tool ? tool.getName() : "Empty"}`);
     });
 }
